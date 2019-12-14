5a077a6b5fee490bc895a88a28b44564eaf5718c
Dinis <dinispeixoto96@gmail.com>
2019-12-14 15:02:26 +0000

    resistor color

---
+++ resistor-color/ResistorColor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+
+public static class ResistorColor
+{
+    public static int ColorCode(string color) => ResistorColor
+        .Colors()
+        .Select((_color, index) => new { _color, index })
+        .First(item => item._color.Equals(color)).index;
+
+    public static string[] Colors() => new[] { "black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white" };
+}
