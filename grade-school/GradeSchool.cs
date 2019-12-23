f5a5215651cc1defa4570871f17988ca92afd580
Dinis <dinispeixoto96@gmail.com>
2019-12-23 22:44:46 +0000

    using sorted collections

--- grade-school/GradeSchool.cs
+++ grade-school/GradeSchool.cs
@@ -4,16 +4,15 @@ using System.Linq;
 
 public class GradeSchool
 {
-    private Dictionary<int, ICollection<string>> students = new Dictionary<int, ICollection<string>>();
+    private IDictionary<int, ICollection<string>> students = new SortedDictionary<int, ICollection<string>>();
 
     public void Add(string student, int grade){
         students.TryGetValue(grade, out var gradeStudents);
-        (gradeStudents ??= new List<string>()).Add(student);
+        (gradeStudents ??= new SortedSet<string>()).Add(student);
         students[grade] = gradeStudents;
     }
 
-    public IEnumerable<string> Roster() => students.Keys.OrderBy(key => key).SelectMany(Grade);
+    public IEnumerable<string> Roster() => students.Keys.SelectMany(Grade);
 
-    public IEnumerable<string> Grade(int grade) => students.TryGetValue(grade, out var gradeStudents) ? 
-        gradeStudents.OrderBy(student => student) : Enumerable.Empty<string>();
+    public IEnumerable<string> Grade(int grade) => students.TryGetValue(grade, out var gradeStudents) ? gradeStudents : Enumerable.Empty<string>();
 }
