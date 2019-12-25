a69406b54d4c51b8026bfaed22b658f72df9cbea
Dinis <dinispeixoto96@gmail.com>
2019-12-25 20:10:30 +0000

    circular buffer

---
+++ circular-buffer/CircularBuffer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public class CircularBuffer<T>
+{
+    private readonly int capacity;
+    private readonly Queue<T> buffer;
+
+    public CircularBuffer(int capacity) {
+        this.capacity = capacity;
+        this.buffer = new Queue<T>(capacity);
+    }
+
+    public T Read() => buffer.Dequeue();
+
+    public void Write(T value) {
+        if (buffer.Count == capacity) throw new InvalidOperationException();
+        buffer.Enqueue(value);
+    }
+
+    public void Overwrite(T value){
+        if (buffer.Count == capacity) buffer.Dequeue();
+        this.Write(value);
+    }
+
+    public void Clear() => buffer.Clear();
+}
