2497f4b8abfb4f2b9cea0c8e92a26690fcb4af5a
Dinis <dinispeixoto96@gmail.com>
2020-01-07 20:23:04 +0000

    spiral matrix

---
+++ spiral-matrix/SpiralMatrix.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class SpiralMatrix
+{
+    public static int[,] GetMatrix(int size)
+    {
+        var counter = 1;
+        var result = new int[size, size];
+
+        int start_row = 0, start_column = 0, end_row = size - 1, end_column = size - 1;
+
+        if(size == 1) return new int[,] {{counter}};
+
+        while (start_row < end_row && start_column < end_column){
+
+            for(int i = start_column; i < end_column; i++) result[start_row, i] = counter++;
+
+            for(int i = start_row; i < end_row; i++) result[i, end_column] = counter++;
+
+            for(int i = end_column; i > start_column; i--) result[end_row, i] = counter++;
+
+            for(int i = end_row; i > start_row; i--) result[i, start_column] = counter++;
+
+            start_row++; start_column++; end_row--; end_column--;
+        }
+
+        if (size % 2 != 0) result[start_row, start_column] = counter;
+
+        return result;
+    }
+}
