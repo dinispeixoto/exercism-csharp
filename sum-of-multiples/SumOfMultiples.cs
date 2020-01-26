5d21a0e7e58ea5bd4ac677714925582c340f1cc1
Dinis <dinispeixoto96@gmail.com>
2020-01-26 20:59:35 +0000

    sum of multiples

---
+++ sum-of-multiples/SumOfMultiples.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public static class SumOfMultiples
+{
+    public static int Sum(IEnumerable<int> multiples, int max) => multiples
+        .Where(multiple => multiple != 0)
+        .SelectMany(multiple => Enumerable.Range(0, max).Where(number => number % multiple == 0)).Distinct()
+        .Sum(number => number);
+}
