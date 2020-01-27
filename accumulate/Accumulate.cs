0dceda507d977626d2a671674acc47dbe9ce8975
Dinis <dinispeixoto96@gmail.com>
2020-01-27 21:45:54 +0000

    accumulate

---
+++ accumulate/Accumulate.cs
@@ -0,0 +1,8 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public static class AccumulateExtensions
+{
+    public static IEnumerable<U> Accumulate<T, U>(this IEnumerable<T> collection, Func<T, U> func) => collection.Select(func);
+}
