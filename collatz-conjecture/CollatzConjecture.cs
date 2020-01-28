c49bec029c8bea435f1f8f515a6b473d0cfa4d6b
Dinis <dinispeixoto96@gmail.com>
2020-01-28 23:00:39 +0000

    steps fixed

--- collatz-conjecture/CollatzConjecture.cs
+++ collatz-conjecture/CollatzConjecture.cs
@@ -2,18 +2,14 @@
 
 public static class CollatzConjecture
 {
-    private const int INITIAL_STEPS = 0;
-
-    public static int Steps(int number) => _Steps(number, INITIAL_STEPS);
-
-    private static int _Steps(int number, int steps)
+    public static int Steps(int number)
     {
-        if (number == 1) return steps;
         if (number < 1) throw new ArgumentException();
 
         Func<int, int> even_operation = (n) => n / 2;
         Func<int, int> odd_operation = (n) => 3 * n + 1;
 
-        return _Steps(number % 2 == 0 ? even_operation(number) : odd_operation(number), ++steps);
+        return (number == 1) ? 
+            0 : Steps(number % 2 == 0 ? even_operation(number) : odd_operation(number)) + 1;
     }
 }
