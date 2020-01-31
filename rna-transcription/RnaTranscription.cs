7b453e8271cf9887b35435012400f04a17ff9d29
Dinis <dinispeixoto96@gmail.com>
2020-01-31 21:50:23 +0000

    updated rna transcription

--- rna-transcription/RnaTranscription.cs
+++ rna-transcription/RnaTranscription.cs
@@ -3,14 +3,12 @@ using System.Linq;
 
 public static class RnaTranscription
 {
-    public static char NucleotideToRna(this char nucleotide) => nucleotide switch
+    public static string ToRna(string nucleotide) => String.Concat(nucleotide.Select(letter => letter switch
     {
-        'G'     => 'C',
-        'C'     => 'G',
-        'T'     => 'A',
-        'A'     => 'U',
-        _       => ' '
-    };
-
-    public static string ToRna(string nucleotide) => String.Concat(nucleotide.Select(letter => letter.NucleotideToRna()));
+        'G' => 'C',
+        'C' => 'G',
+        'T' => 'A',
+        'A' => 'U',
+        _   => ' '
+    }));
 }
