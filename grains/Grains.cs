a9e95b3cee550e6e538d235ff18496da55d624f2
Dinis <dinispeixoto96@gmail.com>
2020-02-04 22:45:15 +0000

    grains

---
+++ grains/Grains.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+public static class Grains
+{
+    public static ulong Square(int n) => n < 1 || n > 64 ? throw new ArgumentOutOfRangeException() : n == 1 ? 1 : 2 * Square(n-1);
+
+    public static ulong Total() => 2 * Square(64) - 1;
+}
