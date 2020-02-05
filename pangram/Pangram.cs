059280c1b7145671d8cffda50409714601f7ae84
Dinis <dinispeixoto96@gmail.com>
2020-02-05 22:55:32 +0000

    pangram

---
+++ pangram/Pangram.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Linq;
+
+public static class Pangram
+{
+    public static bool IsPangram(string input) => 
+        Enumerable.Range('a', 'z' - 'a' + 1).All(c => input.ToLower().Contains((char)c));
+}
