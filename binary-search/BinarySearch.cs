7c0573650c4b08034991099a14400f42e5669473
Dinis <dinispeixoto96@gmail.com>
2020-02-09 16:23:04 +0000

    binary search

---
+++ binary-search/BinarySearch.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public static class BinarySearch
+{
+    public static int Find(int[] input, int value)
+    {
+
+        var min = 0;
+        var max = input.Length - 1;
+
+        var result = -1;
+       
+        if (input.Length == 0 ) return result;
+
+        while(value >= input[min] && value <= input[max])
+        {
+            var index = (min+max)/2;
+            var tmp_value = input[index];
+
+            if (tmp_value == value)
+            {
+                result = index;
+                break;
+            }
+            else
+            {
+                if (value >= tmp_value)
+                {
+                    min = index + 1;
+                }
+                else
+                {
+                    max = index - 1;
+                }
+            }
+        }
+
+        return result;
+    }
+}
