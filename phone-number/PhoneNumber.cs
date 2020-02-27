abe5de2e6b227954487639535fa76675c90865ea
Dinis <dinispeixoto96@gmail.com>
2020-02-27 19:49:08 +0000

    phone number

--- phone-number/PhoneNumber.cs
+++ phone-number/PhoneNumber.cs
@@ -1,9 +1,16 @@
 using System;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 public class PhoneNumber
 {
+    private static Match MatchNANP(string phoneNumber) => Regex.Match(string.Concat(phoneNumber.Where(char.IsDigit)), @"^1?([2-9]\d\d[2-9]\d{6})$");
+
     public static string Clean(string phoneNumber)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var match = MatchNANP(phoneNumber);
+        
+        return ((match = MatchNANP(phoneNumber)).Success) ? 
+            match.Groups[1].ToString() : throw new ArgumentException();
     }
 }
