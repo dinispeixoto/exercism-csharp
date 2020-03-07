7028576932140a740d511992fa5eef217b53ddb3
Dinis <dinispeixoto96@gmail.com>
2020-03-07 21:48:55 +0000

    proverb

---
+++ proverb/Proverb.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+
+public static class Proverb {
+    private static string Sentence (string firstWord, string secondWord) => $"For want of a {firstWord} the {secondWord} was lost.";
+    private static string LastSentence (string word) => $"And all for the want of a {word}.";
+
+    public static string[] Recite (string[] subjects) => subjects
+        .Select ((element, index) => (index == subjects.Length - 1) ?
+            LastSentence (subjects.First ()) : Sentence (element, subjects[index + 1]))
+        .ToArray ();
+}
