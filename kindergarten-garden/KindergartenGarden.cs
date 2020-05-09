9d18bb92fb1b32c86b5f617d2ba5b37068b56ee7
Dinis <dinispeixoto96@gmail.com>
2020-05-09 19:31:35 +0100

    kindergarten garden

---
+++ kindergarten-garden/KindergartenGarden.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public enum Plant
+{
+    Violets = 'V',
+    Radishes = 'R',
+    Clover = 'C',
+    Grass = 'G'
+}
+
+public class KindergartenGarden
+{
+    private readonly IEnumerable<string> garden;
+    private readonly IEnumerable<string> students = new List<string> {
+        "Alice", "Bob", "Charlie", "David", "Eve", "Fred", "Ginny", "Harriet", "Ileana", "Joseph", "Kincaid", "Larry"
+    };
+
+    public KindergartenGarden(string diagram)
+    {
+        this.garden = diagram
+            .Split(new[] { Environment.NewLine }, StringSplitOptions.None)
+            .Select(line => line);
+    }
+
+    public IEnumerable<Plant> Plants(string student)
+    {
+        var index = students.ToList().IndexOf(student);
+
+        var first = (index * 2);
+        var second = (index * 2) + 1;
+
+        return new Plant[] {
+            (Plant)this.garden.ToList()[0][first],
+            (Plant)this.garden.ToList()[0][second],
+            (Plant)this.garden.ToList()[1][first],
+            (Plant)this.garden.ToList()[1][second],
+        };
+    }
+}
