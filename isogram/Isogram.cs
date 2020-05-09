9f40e2ccb811424cd8aee553e7f1490a82352492
Dinis <dinispeixoto96@gmail.com>
2020-05-09 17:52:38 +0100

    isogram

---
+++ isogram/Isogram.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+public static class Isogram
+{
+    public static bool IsIsogram(string word)
+    {
+        var sortedString = String.Concat(word.Where(char.IsLetterOrDigit).OrderBy(c => c)).ToLower();
+
+        for(var i = 0; i < sortedString.Length - 1; i++)
+        {
+            if (sortedString[i] == sortedString[i+1])
+                return false;
+        }
+
+        return true;
+    }
+}
