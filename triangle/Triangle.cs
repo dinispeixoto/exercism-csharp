eca90ed3bc6d350dd5da18c07646257ea8a4d6ff
Dinis <dinispeixoto96@gmail.com>
2020-05-09 11:58:42 +0100

    improve triangle

--- triangle/Triangle.cs
+++ triangle/Triangle.cs
@@ -4,21 +4,18 @@ using System.Linq;
 public static class Triangle
 {
     public static bool IsScalene(double side1, double side2, double side3)
-        => IsValid(side1, side2, side3) && DifferentSides(side1, side2, side3) == 3;
+        => IsValid(side1, side2, side3) && NumberOfDifferentSides(side1, side2, side3) == 3;
 
     public static bool IsIsosceles(double side1, double side2, double side3)
-        => IsValid(side1, side2, side3) && DifferentSides(side1, side2, side3) >= 1 && !IsScalene(side1, side2, side3);
+        => IsValid(side1, side2, side3) && !IsScalene(side1, side2, side3);
 
     public static bool IsEquilateral(double side1, double side2, double side3)
-        => IsValid(side1, side2, side3) && DifferentSides(side1, side2, side3) == 1;
+        => IsValid(side1, side2, side3) && NumberOfDifferentSides(side1, side2, side3) == 1;
 
     public static bool IsValid(double side1, double side2, double side3)
-        => side1 != default && side2 != default && side3 != default && DoesntViolateTriangleInequality(side1, side2, side3);
-
-    public static bool DoesntViolateTriangleInequality(double side1, double side2, double side3)
         => side1 + side2 > side3 && side1 + side3 > side2 && side2 + side3 > side1;
 
-    public static int DifferentSides(double side1, double side2, double side3)
+    public static int NumberOfDifferentSides(double side1, double side2, double side3)
         => new double[] { side1, side2, side3 }.GroupBy(side => side).Count();
 
 }
