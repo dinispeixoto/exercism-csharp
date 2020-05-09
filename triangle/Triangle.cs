a75c51ba3a0f48e5d5401a739fe1a937c43e5beb
Dinis <dinispeixoto96@gmail.com>
2020-05-09 11:51:58 +0100

    triangle

--- triangle/Triangle.cs
+++ triangle/Triangle.cs
@@ -1,19 +1,24 @@
 using System;
+using System.Linq;
 
 public static class Triangle
 {
     public static bool IsScalene(double side1, double side2, double side3)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+        => IsValid(side1, side2, side3) && DifferentSides(side1, side2, side3) == 3;
 
-    public static bool IsIsosceles(double side1, double side2, double side3) 
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+    public static bool IsIsosceles(double side1, double side2, double side3)
+        => IsValid(side1, side2, side3) && DifferentSides(side1, side2, side3) >= 1 && !IsScalene(side1, side2, side3);
 
-    public static bool IsEquilateral(double side1, double side2, double side3) 
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+    public static bool IsEquilateral(double side1, double side2, double side3)
+        => IsValid(side1, side2, side3) && DifferentSides(side1, side2, side3) == 1;
+
+    public static bool IsValid(double side1, double side2, double side3)
+        => side1 != default && side2 != default && side3 != default && DoesntViolateTriangleInequality(side1, side2, side3);
+
+    public static bool DoesntViolateTriangleInequality(double side1, double side2, double side3)
+        => side1 + side2 > side3 && side1 + side3 > side2 && side2 + side3 > side1;
+
+    public static int DifferentSides(double side1, double side2, double side3)
+        => new double[] { side1, side2, side3 }.GroupBy(side => side).Count();
+
 }
